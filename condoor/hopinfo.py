96a11879c7b817607feebc5346c3749466ccc5e7
Klaudiusz Staniek <kstaniek@gmail.com>
2017-02-08 21:59:44 +0100

    Handling quoted username, password and enable password

--- condoor/hopinfo.py
+++ condoor/hopinfo.py
@@ -1,7 +1,7 @@
 """Provides HopInfo class implementation and factory function."""
 
-import re
 from urlparse import urlparse
+from urllib import unquote
 
 from condoor.exceptions import InvalidHopInfoError
 
@@ -40,19 +40,16 @@ def make_hop_info_from_url(url, verify_reachability=None):
 
     """
     parsed = urlparse(url)
-    try:
-        # search for anything which is after the netloc/
-        enable_password = re.search(re.compile(parsed.netloc + "/(.*)"), url).group(1)
-    except AttributeError:  # not found
-        enable_password = None
+    username = None if parsed.username is None else unquote(parsed.username)  # It's None if not exists
+    password = None if parsed.password is None else unquote(parsed.password)  # It's None if not exists
 
     hop_info = HopInfo(
         parsed.scheme,
         parsed.hostname,
-        parsed.username,
-        parsed.password,
+        username,
+        password,
         parsed.port,
-        enable_password,
+        unquote(parsed.path[1:]),  # first char is '/'. It's empty if not exists in url
         verify_reachability=verify_reachability
     )
     if hop_info.is_valid():
